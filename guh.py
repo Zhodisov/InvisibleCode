d9d6671a9a7df1fc5f9378fbdd2685db9e5e6a39
Zhodiyn <132891555+Zhodisov@users.noreply.github.com>
2024-10-02 21:26:09 +0400

    Create guh.py

---
+++ guh.py
@@ -0,0 +1,15 @@
+class Safemarket:
+    ᅟ = "HANGUL CHOSEONG FILLER"
+    ﾠ = "HALFWIDTH HANGUL FILLER"
+    ㅤ = "HANGUL FILLER"
+    ᅠ = "HANGUL JUNGSEONG FILLER"
+    print(f"{ᅟ}")
+    print(f"{ﾠ}")
+    print(f"{ㅤ}")
+    print(f"{ᅠ}")
+#print(Safemarket)
+#==============================================================================
+ᅟ = "import"
+ᅟᅟ = "requests"
+ᅟᅟ
+print(ᅟ)
